76a0e6a3540f69fa80831e1b1ea14ca504b7d12a
Jan Vrany <jan.vrany@labware.com>
2025-12-16 10:38:36 +0000

    gdb/python: allow instantiation of gdb.Objfile from Python
    This commit adds code to allow user extension to instantiate
    gdb.Objfile. This is a step towards a Python support for dynamically
    generated code (JIT) in GDB.
    
    Reviewed-By: Eli Zaretskii <eliz@gnu.org>

--- gdb/python/python-internal.h
+++ gdb/python/python-internal.h
@@ -535,6 +535,7 @@ struct symtab_and_line *sal_object_to_symtab_and_line (PyObject *obj);
 frame_info_ptr frame_object_to_frame_info (PyObject *frame_obj);
 struct gdbarch *arch_object_to_gdbarch (PyObject *obj);
 struct compunit_symtab *compunit_object_to_compunit (PyObject *obj);
+inferior *inferior_object_to_inferior(PyObject *obj);
 
 /* Return true if OBJ is a gdb.Style object.  OBJ must not be NULL.  */
 
